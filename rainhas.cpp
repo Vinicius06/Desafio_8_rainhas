7fbe74e40d6892ee7cffe952053a636574c35fdd
Vinicius <vinicius.camargo.bandeira@gmail.com>
2023-06-06 19:29:15 -0300

    segundo commit

--- rainhas.cpp
+++ rainhas.cpp
@@ -1,8 +1,9 @@
 #include <vector>
 #include "rainhas.hpp"
 #include <algorithm>
+#include <fstream>
 
-bool verificar_posicao(const std::vector<std::vector<int>>& tabuleiro, int linha, int col) {
+bool posicao_segura(const std::vector<std::vector<int>>& tabuleiro, int linha, int col) {
     int n = tabuleiro.size();
 
     // Verifica se há uma rainha na mesma coluna
@@ -26,26 +27,53 @@ bool verificar_posicao(const std::vector<std::vector<int>>& tabuleiro, int linha
     return true;
 }
 
+void ataques_rainhas(const std::vector<std::vector<int>>& tabuleiro) {
+    std::ofstream outputFile("ataques_rainhas.txt");
+    int n = tabuleiro.size();
+
+    // Percorre todas as posições do tabuleiro
+    for (int linha = 0; linha < n; linha++) {
+        for (int col = 0; col < n; col++) {
+            // Verifica se há uma rainha na posição (linha, col)
+            if (tabuleiro[linha][col] == 1) {
+                // Verifica se ela se ataca com outras rainhas
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (i != linha && j != col) {
+                            if (tabuleiro[i][j] == 1 && !posicao_segura(tabuleiro, i, j)) {
+                                // Escreve as posições das rainhas que se atacam no arquivo
+                                outputFile << "(" << linha << "," << col << ") ataca (" << i << "," << j << ")\n";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    outputFile.close();
+}
+
 int checar_solucao(const std::vector<int>& tabuleiro) {
     int n = tabuleiro.size();
     if (n != 64) {
         return -1;
     }
 
-    std::vector<std::vector<int>> chessboard(8, std::vector<int>(8, 0));
+    std::vector<std::vector<int>> xadrez(8, std::vector<int>(8, 0));
 
-    // Preenche o tabuleiro com base no vetor board
+    // Preenche o tabuleiro com base no vetor tabuleiro
     for (int i = 0; i < n; i++) {
         int linha = i / 8;
         int col = i % 8;
-        chessboard[linha][col] = tabuleiro[i];
+        xadrez[linha][col] = tabuleiro[i];
     }
 
     // Verifica se há mais de 8 rainhas
-    int numRainhas = 0;
-    for (const auto& linha : chessboard) {
-        numRainhas += std::count(linha.begin(), linha.end(), 1);
-        if (numRainhas > 8) {
+    int numQueens = 0;
+    for ( const auto& linha : xadrez) {
+        numQueens += std::count(linha.begin(), linha.end(), 1);
+        if (numQueens > 8) {
             return -1;
         }
     }
@@ -53,7 +81,9 @@ int checar_solucao(const std::vector<int>& tabuleiro) {
     // Verifica se as rainhas estão em posições seguras
     for (int linha = 0; linha < 8; linha++) {
         for (int col = 0; col < 8; col++) {
-            if (chessboard[linha][col] == 1 && !verificar_posicao(chessboard, linha, col)) {
+            if (xadrez[linha][col] == 1 && !posicao_segura(xadrez, linha, col)) {
+                // Rainhas se atacam, escreve no arquivo de saída
+                ataques_rainhas(xadrez);
                 return 0;
             }
         }
@@ -61,3 +91,4 @@ int checar_solucao(const std::vector<int>& tabuleiro) {
 
     return 1;
 }
+
--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -18,50 +18,6 @@ TEST_CASE("Teste de solução válida das 8 damas") {
     REQUIRE(checar_solucao(tabuleiro) == 1);
 }
 
-TEST_CASE("Teste de solução válida das 8 damas v2") {
-    std::vector<int> tabuleiro = {
-        0, 0, 0, 1, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 1, 0,
-        0, 0, 1, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 1,
-        0, 1, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 1, 0, 0, 0,
-        1, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 1, 0, 0
-    };
-
-    REQUIRE(checar_solucao(tabuleiro) == 1);
-}
-
-TEST_CASE("Teste de solução válida das 8 damas v3") {
-    std::vector<int> tabuleiro = {
-        0, 0, 0, 0, 1, 0, 0, 0,
-        0, 1, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 1, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 1, 0,
-        0, 0, 1, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 1,
-        0, 0, 0, 0, 0, 1, 0, 0,
-        1, 0, 0, 0, 0, 0, 0, 0
-    };
-
-    REQUIRE(checar_solucao(tabuleiro) == 1);
-}
-
-TEST_CASE("Teste de solução válida das 8 damas v4") {
-    std::vector<int> tabuleiro = {
-        0, 0, 0, 1, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 1, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 1,
-        0, 0, 1, 0, 0, 0, 0, 0,
-        1, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 1, 0,
-        0, 0, 0, 0, 1, 0, 0, 0,
-        0, 1, 0, 0, 0, 0, 0, 0
-    };
-
-    REQUIRE(checar_solucao(tabuleiro) == 1);
-}
 
 TEST_CASE("Teste de validação do tabuleiro inválido: solução inválida") {
     std::vector<int> tabuleiro = {
@@ -78,35 +34,7 @@ TEST_CASE("Teste de validação do tabuleiro inválido: solução inválida") {
     REQUIRE(checar_solucao(tabuleiro) == 0);
 }
 
-TEST_CASE("Teste de validação do tabuleiro inválido: solução inválida v2") {
-    std::vector<int> tabuleiro = {
-        1, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 1, 0, 0, 0, 
-        0, 0, 0, 0, 0, 0, 0, 1,
-        0, 0, 0, 0, 0, 1, 0, 0,
-        0, 0, 1, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 1, 0,
-        0, 1, 0, 0, 0, 0, 0, 0,
-        0, 1, 0, 0, 0, 0, 0, 0
-    };
 
-    REQUIRE(checar_solucao(tabuleiro) == 0);
-}
-
-TEST_CASE("Teste de validação do tabuleiro inválido: solução inválida v3") {
-    std::vector<int> tabuleiro = {
-        1, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 1, 0, 0, 0, 
-        0, 0, 0, 0, 0, 0, 0, 1,
-        0, 0, 0, 0, 0, 1, 0, 0,
-        0, 0, 1, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 1, 0,
-        0, 1, 0, 0, 0, 0, 0, 0,
-        0, 0, 1, 0, 0, 0, 0, 0
-    };
-
-    REQUIRE(checar_solucao(tabuleiro) == 0);
-}
 
 TEST_CASE("Teste de solução inválida do tabuleiro ou rainhas") {
     std::vector<int> tabuleiro = {
