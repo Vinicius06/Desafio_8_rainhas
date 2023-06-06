6a04005b0a3bed2b623b61782fde9c3bae837938
Vinicius <vinicius.camargo.bandeira@gmail.com>
2023-06-06 19:57:31 -0300

    13° commit

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -123,6 +123,21 @@ TEST_CASE("Oitavo Teste de solução válida das 8 damas") {
     REQUIRE(checar_solucao(tabuleiro) == 1);
 }
 
+TEST_CASE("Nono Teste de solução válida das 8 damas") {
+    std::vector<int> tabuleiro = {
+        0, 0, 1, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0,
+        0, 1, 0, 0, 0, 0, 0, 0
+    };
+
+    REQUIRE(checar_solucao(tabuleiro) == 1);
+}
+
 TEST_CASE("Teste de validação do tabuleiro inválido: solução inválida") {
     std::vector<int> tabuleiro = {
         1, 0, 0, 0, 0, 0, 0, 0,
